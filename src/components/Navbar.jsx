9f832fc20c8e1f500bfa56095d8786b33f382de0
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-06-09 13:56:30 +0100

    added about

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,25 +34,28 @@ const Navbar = () => {
   ];
   return (
     <div>
-      <div className="flex     px-4  flex-row py-8 text-black item-center  justify-between   ">
-        <h2 className="text-3xl font-extrabold cursor-pointer ">VSDA</h2>
-        <div className="flex gap-4  pt-10 font ">
-          {menus &&
-            menus.length > 1 &&
-            menus.map((menu, index) => {
-              return (
-                <NavLink
-                  to={menu.link}
-                  className={({ isActive, isPending }) =>
-                    isActive
-                      ? "after:h-1  after:bg-blue-400  after:absolute relative  after:w-full after:-bottom-4 after:left-0  after:content-['']"
-                      : "font-light"
-                  }
-                >
-                  <p>{menu.name}</p>
-                </NavLink>
-              );
-            })}
+      <div className="flex     px-4  flex-row py-8 text-black item-center  justify-between  sticky  z-50 ">
+        <div className="flex gap-4  pt-10 font  flex flex-row w-full   items-center justify-between pl-16">
+          <h2 className="text-3xl font-extrabold cursor-pointer ">VSDA</h2>
+
+          <div className="flex  gap-4 px-24">
+            {menus &&
+              menus.length > 1 &&
+              menus.map((menu, index) => {
+                return (
+                  <NavLink
+                    to={menu.link}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "after:h-1  after:bg-blue-400  after:absolute relative  after:w-full after:-bottom-4 after:left-0  after:content-['']"
+                        : "font-light"
+                    }
+                  >
+                    <p>{menu.name}</p>
+                  </NavLink>
+                );
+              })}
+          </div>
         </div>
       </div>
     </div>
