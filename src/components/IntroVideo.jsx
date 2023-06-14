5839a595c59d8474fa64d4666b52af6a2652ba6c
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-06-14 12:24:22 +0100

    fix

--- src/components/IntroVideo.jsx
+++ src/components/IntroVideo.jsx
@@ -3,8 +3,8 @@ import { Player } from "video-react";
 
 const IntroVideo = () => {
   return (
-    <div className="h-2/4 py-10 px-7 my-10  flex items-center justify-between">
-      <div className="flex flex-col gap-8 w-2/5">
+    <div className="h-2/4 py-10 px-10 my-10  flex items-center justify-between md:flex-row flex-col">
+      <div className="flex  flex-col gap-8 md:w-2/5 w-full py-5">
         <p className="font-black text-5xl ">Learn with Engr. Gabby</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
@@ -14,14 +14,9 @@ const IntroVideo = () => {
         </p>
       </div>
 
-      <div>
-        <video controls>
-          <source
-            src={require("../assets/videos/intro.mp4")}
-            type="video/mp4"
-          />
-        </video>
-      </div>
+      <video controls>
+        <source src={require("../assets/videos/intro.mp4")} type="video/mp4" />
+      </video>
 
       {/* <Player>
         <source src={require("../assets/videos/intro.mp4")} />
