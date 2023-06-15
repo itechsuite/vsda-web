c9a5909679bfc34e6be772261572fbeba2621f6a
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-06-15 01:11:43 +0100

    contact

---
+++ src/components/HeadOfficeMap.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HeadOfficeMap = () => {
+  return (
+    <div className="flex justify-center">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.660638129738!2d7.0669074!3d4.828204499999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1069cd172742b501%3A0xc5bbf87d34d5c2ef!2s18%20Trans%20Woji%20Rd%2C%20Rumuwaji%20500102%2C%20Port%20Harcourt%2C%20Rivers!5e0!3m2!1sen!2sng!4v1686763761152!5m2!1sen!2sng"
+        width="70%"
+        height="450"
+        // style="border:0;"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
+export default HeadOfficeMap;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ContactForm from "../components/ContactForm";
 import Banner from "../components/Banner";
 import GetInTouch from "../components/GetInTouch";
+import HeadOfficeMap from "../components/HeadOfficeMap";
 
 const Contact = () => {
   return (
     <section className="min-h-[80vh]  px-10 py-10 ">
       {/* <Banner /> */}
+
+      <HeadOfficeMap />
 
       <GetInTouch />
     </section>
