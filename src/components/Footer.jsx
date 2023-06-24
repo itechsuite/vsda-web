413efa55602b35d5776dc4eaae8c25f2628000d6
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-06-24 10:42:18 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer p-10 bg-[#252933] text-base-content text-white">
       <div>
         <span className="footer-title">Services</span>
-        <a className="link link-hover">Skills Market Place </a>
-        <a className="link link-hover">Online Store</a>
-        <a className="link link-hover">Courses</a>
+        <Link to={"/skills"} className="link link-hover">
+          Skills Market Place{" "}
+        </Link>
+        <Link to={"/online"} className="link link-hover">
+          Online Store
+        </Link>
+        <Link to={"/courses"} className="link link-hover">
+          Courses
+        </Link>
       </div>
       <div>
         <span className="footer-title">Company</span>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
+        <Link to={"/about-us"} className="link link-hover">
+          About us
+        </Link>
+        <Link to={"contact"} className="link link-hover">
+          Contact
+        </Link>
       </div>
       {/* <div>
         <span className="footer-title">Legal</span>
